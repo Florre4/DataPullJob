e76380a9954648ee10c785800389085832a0f53e
Florian Vromant <florian4_9@hotmail.com>
2018-06-24 15:57:36 +0000

    Finished the data pull job, displays the new data, can post it to OP, and clean up in cloud

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,4 +1,4 @@
-/*eslint-disable no-console, no-alert */
+/*eslint-disable no-console, no-alert, no-loop-func*/
 sap.ui.define([
 	"sap/m/MessageToast",
 	"sap/ui/core/mvc/Controller",
@@ -8,39 +8,91 @@ sap.ui.define([
 
 	return Controller.extend("DataPullJob.controller.View1", {
 
-		onPress: function(evt) {
-
+		getData: function(evt) {
+			//create cloud Odata model
 			var OModelCloud = new sap.ui.model.odata.v2.ODataModel("/com/wieiswie/v1/wiw.xsodata");
+			//Bind it to app
+			sap.ui.getCore().setModel(OModelCloud, "cloudModel");
 			var mandtFilter = new sap.ui.model.Filter({
-                     path: "MANDT",
-                     operator: sap.ui.model.FilterOperator.EQ,
-                     value1: "000"
-              });
+				path: "MANDT",
+				operator: sap.ui.model.FilterOperator.EQ,
+				value1: "000"
+			});
+			//Read the filtered data
+
 			OModelCloud.read("/Person", {
 				filters: [mandtFilter],
 				success: function(oRetrievedResult) {
-					var OModelOnPremise = new sap.ui.model.odata.v2.ODataModel("/sap/opu/odata/sap/ZHS_WIW_ODATA_SRV/");
-					OModelOnPremise.setUseBatch(false);
-					for (var i = 0; i < oRetrievedResult.results.length; i++) {
-						delete oRetrievedResult.results[i].__metadata;
-						OModelOnPremise.create("/Person", oRetrievedResult.results[i]);
-						//OModelOnPremise.submitChanges();
-					}
-
+					//set the data to the json model
+					sap.ui.getCore().getModel("jsonModel").setData(oRetrievedResult);
 				},
-				error: function(oError) { console.log(oError); 
+				error: function(oError) {
+					console.log(oError);
 				}
 			});
+			//display the new data in the list
+			var olist = this.getView().byId("personList");
+			olist.setModel(sap.ui.getCore().getModel("jsonModel"));
+			olist.bindItems({
+				path: "/results",
+				template: new sap.m.ObjectListItem({
+					title: "{FAMNAAM} {VOORNAAM}",
+					attributes: [new sap.m.ObjectAttribute({
+							text: "Mandt: {MANDT}"
+						}),
+						new sap.m.ObjectAttribute({
+							text: "HoofdPerNr: {HOOFDPERNR}"
+						})
+					]
+				})
+			});
 
-			MessageToast.show(evt.getSource().getId() + " Pressed");
+			MessageToast.show("People retrieved");
 		},
 
 		onInit: function(oEvent) {
-
+			//Local json model to hold the data
+			var jModel1 = new sap.ui.model.json.JSONModel();
+			sap.ui.getCore().setModel(jModel1, "jsonModel");
 		},
 
-		doCleanUp: function(oModel) {
-		
+		postData: function(evt) {
+			//Create the on premise odata model
+			var OModelOnPremise = new sap.ui.model.odata.v2.ODataModel("/sap/opu/odata/sap/ZHS_WIW_ODATA_SRV/");
+			OModelOnPremise.setUseBatch(false); //batch mode not supported
+			//bind it to the app
+			sap.ui.getCore().setModel(OModelOnPremise, "onPremiseModel");
+			//get the json model with the data
+			var data = sap.ui.getCore().getModel("jsonModel").getData();
+			for (var i = 0; i < data.results.length; i++) {
+				//Metadata would give an error
+				delete data.results[i].__metadata;
+				OModelOnPremise.create("/Person", data.results[i], {
+					success: function(oCreatedEntry) {
+						MessageToast.show("Entries posted succesfully");
+					},
+					error: function(oError) {
+						MessageToast.show("ERROR: entry most likely already existed, check console");
+						console.log(oError);
+					}
+
+				});
+			}
+		},
+
+		onCleanUp: function(oEvent) {
+			var oModelCloud = sap.ui.getCore().getModel("cloudModel");
+			var data = sap.ui.getCore().getModel("jsonModel").getData();
+			for (var i = 0; i < data.results.length; i++) {
+				var mandt, pernr;
+				mandt = data.results[i].MANDT;
+				pernr = data.results[i].HOOFDPERNR;
+					oModelCloud.remove("/Person(MANDT='" + mandt + "',HOOFDPERNR='" + pernr + "')", {
+						success: function() { MessageToast.show("Entries deleted succesfully"); },
+						error: function(oError) { MessageToast.show("ERROR: entry not found, check console"); }
+					});
+			}
+			//sap.ui.getCore().getModel("jsonModel").setData(null); 
 		}
 	});
 });
